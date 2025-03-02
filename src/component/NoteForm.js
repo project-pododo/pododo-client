35401498c4b4792c5c0cc87ed1d541a17c6276c6
KimYJ <aidenkim1991>
2025-03-02 23:14:16 +0900

    할일 등록 api 추가 커밋

--- src/component/NoteForm.js
+++ src/component/NoteForm.js
@@ -22,24 +22,27 @@ function NoteForm({ onAdd }) {
 
   // API 호출 함수
   const handleApiCall = async () => {
+    if (!title.trim() || !content.trim() || !dateRange) {
+      message.warning("필수 입력값을 입력해주세요.");
+      return;
+    }
     setLoading(true);
 
     try {
-      // API 호출 (예시: POST 요청)
       const response = await axios.post(
-        "http://35.216.16.197:8081/api/v1/todo",
+        "/api/v1/todo",
         {
-          "todoName": "yongjoon",
-          "todoDetail": "This is a test todo task.",
-          "startDate": "2025-02-24 09:00",
-          "endDate": "2025-02-25 09:00"
+          todoName: title,
+          todoDetail: content,
+          startDate: dateRange[0].format("YYYY-MM-DD HH:mm"),
+          endDate: dateRange[1].format("YYYY-MM-DD HH:mm"),
         }
       );
 
-      if (response.data) {
-        message.success("API 호출 성공!");
+      if (response.data && response.data.message) {
+        message.success(response.data.message);
       } else {
-        message.error("API 호출 실패.");
+        message.error(response.data.message);
       }
     } catch (error) {
       message.error("API 호출 중 오류 발생.");
@@ -47,31 +50,6 @@ function NoteForm({ onAdd }) {
     } finally {
       setLoading(false);
     }
-  };
-
-  const handleAdd = () => {
-    if (!title.trim()) {
-      message.warning("제목을 입력해주세요. - CD");
-      return;
-    }
-
-    if (!content.trim()) {
-      message.warning("내용을 입력해주세요.");
-      return;
-    }
-
-    setLoading(true);
-
-    setTimeout(() => {
-      const finalDateRange = dateRange || [defaultStart, defaultEnd];
-
-      onAdd(title, content, finalDateRange);
-      message.success("일정이 추가되었습니다.");
-      setTitle("");
-      setContent("");
-      setDateRange([defaultStart, defaultEnd]);
-      setLoading(false);
-    }, 1000); //임시로 로딩시간 1초 세팅. api 적용 후 제거.
   };
 
   return (
@@ -109,7 +87,7 @@ function NoteForm({ onAdd }) {
         <Spin spinning={loading} tip="Loading...">
           <Button
             type="primary"
-            onClick={handleAdd}
+            onClick={handleApiCall}
             style={{
               marginTop: 8,
               width: "100%",
@@ -119,19 +97,6 @@ function NoteForm({ onAdd }) {
             loading={loading}
           >
             추가하기
-          </Button>
-          <Button
-            type="default"
-            onClick={handleApiCall}
-            style={{
-              marginTop: 8,
-              width: "100%",
-              backgroundColor: "#4CAF50",
-              borderColor: "#4CAF50",
-            }}
-            loading={loading}
-          >
-            API 호출하기
           </Button>
         </Spin>
       </div>
