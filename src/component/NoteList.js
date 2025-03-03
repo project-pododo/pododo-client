26b237397667d066a7f6cf5f0d85519091a48f41
KimYJ <aidenkim1991>
2025-03-03 23:08:44 +0900

    조회 api 추가 및 상태변경 수정 중간커밋

--- src/component/NoteList.js
+++ src/component/NoteList.js
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BackTop, message } from "antd";
 import NoteCard from "./NoteCard";
 import dayjs from "dayjs";
 import axios from "axios";
 
-function NoteList({ notes, onDelete, onUpdate, onOverdueChange }) {
-  const pendingNotes = notes.filter((note) => !note.isCompleted);
-  const completedNotes = notes.filter((note) => {
-    if (!note.isCompleted || !note.dateRange) return false;
-    return dayjs().diff(dayjs(note.dateRange[1]), "day") < 1;
-  });
+function NoteList({ onDelete, onUpdate, onOverdueChange }) {
+  const [notes, setNotes] = useState([]);
+  const [completedNotes, setCompletedNotes] = useState([]);
 
+  // 투두 조회
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get("/api/v1/todo");
+
+      if (response.data && response.data.code === "10000") {
+        const formattedNotes = response.data.data.map((item) => ({
+          id: item.todoMstId,
+          isCompleted: item.todoStatus === "DONE",
+          title: item.todoName,
+          content: item.todoDetail,
+          dateRange: [dayjs(item.startDate), dayjs(item.endDate)],
+        }));
+        setNotes(formattedNotes);
+      } else {
+        message.error(response.data.message || "리스트를 불러오는 데 실패했습니다.");
+      }
+    } catch (error) {
+      message.error("할 일 목록을 불러오는 중 오류 발생.");
+      console.error("Error:", error);
+    }
+  };
+
+  // 완료 조회
+  const fetchCompletedNotes = async () => {
+    const today = dayjs().format("YYYY-MM-DD");
+    // const startDate = today;
+    const startDate = '2025-01-01';
+    const endDate = today;
+
+    try {
+      const response = await axios.get(`/api/v1/todo/completed`, {
+        params: { startDate, endDate },
+      });
+
+      if (response.data && response.data.code === "10000") {
+        const formattedCompletedNotes = response.data.data.map((item) => ({
+          id: item.todoMstId,
+          isCompleted: item.todoStatus === "DONE",
+          title: item.todoName,
+          content: item.todoDetail,
+          dateRange: [dayjs(item.startDate), dayjs(item.endDate)],
+        }));
+        setCompletedNotes(formattedCompletedNotes);
+      } else {
+        message.error(response.data.message || "완료된 목록을 불러오는 데 실패했습니다.");
+      }
+    } catch (error) {
+      message.error("완료된 목록을 불러오는 중 오류 발생.");
+      console.error("Error:", error);
+    }
+  };
+
+  // 페이지 로드 시 api호출
+  useEffect(() => {
+    fetchNotes();
+    fetchCompletedNotes();
+  }, []);
+
+  // 투두 업데이트 API
   const handleUpdate = async (updatedNote) => {
     try {
       const response = await axios.put("/api/v1/todo", {
@@ -23,7 +80,8 @@ function NoteList({ notes, onDelete, onUpdate, onOverdueChange }) {
 
       if (response.data && response.data.message) {
         message.success(response.data.message);
-        onUpdate(updatedNote);
+        await fetchNotes(); 
+        await fetchCompletedNotes();
       } else {
         message.error(response.data.message);
       }
@@ -43,14 +101,16 @@ function NoteList({ notes, onDelete, onUpdate, onOverdueChange }) {
       }}
     >
       <h2>할 일</h2>
-      {pendingNotes.length > 0 ? (
-        pendingNotes.map((note) => (
+      {notes.length > 0 ? (
+        notes.map((note) => (
           <NoteCard
             key={note.id}
             note={note}
             onDelete={onDelete}
             onUpdate={handleUpdate}
             onOverdueChange={onOverdueChange}
+            fetchNotes={fetchNotes}
+            fetchCompletedNotes={fetchCompletedNotes}
           />
         ))
       ) : (
@@ -67,6 +127,8 @@ function NoteList({ notes, onDelete, onUpdate, onOverdueChange }) {
             onDelete={onDelete}
             onUpdate={handleUpdate}
             onOverdueChange={onOverdueChange}
+            fetchNotes={fetchNotes}
+            fetchCompletedNotes={fetchCompletedNotes}
           />
         ))
       ) : (
