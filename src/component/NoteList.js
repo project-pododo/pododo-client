c50945f826ef7398695eec0d36b6ee6e75a06a01
KimYJ <aidenkim1991>
2025-03-27 21:29:22 +0900

    api 수정

--- src/component/NoteList.js
+++ src/component/NoteList.js
@@ -38,7 +38,7 @@ function NoteList({ onDelete }) {
   // 투두 조회
   const fetchNotes = async () => {
     try {
-      const response = await axios.get("/api/v1/todo");
+      const response = await axios.get("http://35.216.16.197:8081/api/v1/todo");
 
       if (response.data && response.data.code === "10000") {
         const formattedNotes = response.data.data
@@ -63,7 +63,7 @@ function NoteList({ onDelete }) {
   // 완료 조회
   const fetchCompletedNotes = async () => {
     try {
-      const response = await axios.get("/api/v1/todo/completed", {
+      const response = await axios.get("http://35.216.16.197:8081/api/v1/todo/completed", {
         params: {
           startDate: "2025-01-01",
           endDate: dayjs().format("YYYY-MM-DD"),
@@ -111,7 +111,7 @@ function NoteList({ onDelete }) {
     setLoading(true);
 
     try {
-      const response = await axios.post("/api/v1/todo", {
+      const response = await axios.post("http://35.216.16.197:8081/api/v1/todo", {
         todoName: newNoteTitle,
         todoDetail: newNoteContent,
         startDate: dateRange[0].format("YYYY-MM-DD HH:mm"),
